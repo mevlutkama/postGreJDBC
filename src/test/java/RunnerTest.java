ad5e7d1d408fa4b303a8f4dfdda7da6a8ecf94be
Mevlut Kama <mtkama02@gmail.com>
2022-12-09 21:13:40 +0300

    JDBC

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,24 @@
+import jdk.jshell.execution.Util;
+
+public class RunnerTest {
+
+    public static void main(String[] args) {
+        // 1.Step: Registration to the Driver
+        // 2.Step: Create connection with database
+        Utils.connectToDatabase("localhost", "Techpro", "postgres", "13265915248");
+
+        // 3.Step: Create statement
+        Utils.createStatement();
+
+        // 4.Step: Execute the query
+        Utils.createTable("school", "teachers varchar(50)", "name varchar(10)", "schoolId numeric(3)");
+
+        Utils.insertIntoTable("school", "name 'John'");
+
+        Utils.dropTable("school");
+
+        // 5.Step: Close the connection and statement
+        Utils.closeConnectionAndStatement();
+
+    }
+}
---
+++ src/test/java/Utils.java
@@ -0,0 +1,136 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Utils {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static Connection connectToDatabase(String hostName, String dataBaseName, String userName, String password) {
+
+        // 1.Step: Registration to the Driver
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 2.Step: Create connection with database
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://" + hostName + ":5432/" + dataBaseName, userName, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Connection is successful!");
+        return connection;
+    }
+
+    public static Statement createStatement() {
+
+        // 3.Step: Create statement
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Statement created!");
+
+        return statement;
+    }
+
+    // 4.Step: Execute the query
+    public static boolean execute(String query) {
+
+        boolean isExecuted = false;
+
+        try {
+            isExecuted = statement.execute(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Command executed successfully");
+        return isExecuted;
+    }
+
+    // 5.Step: Close the connection and statement
+    public static void closeConnectionAndStatement() {
+
+        try {
+            connection.close();
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            if (connection.isClosed() && statement.isClosed()) {
+                System.out.println("Connection and statement closed");
+            } else {
+                System.out.println("Connection and statement are not closed!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void dropTable(String tableName){
+        try {
+            statement.execute("drop table " + tableName);
+            System.out.println("Table " + tableName + " dropped!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void createTable(String tableName, String... columnName_DataType ){
+
+        StringBuilder columnName_DataTypeString  = new StringBuilder("");
+        for (String w: columnName_DataType){
+            columnName_DataTypeString.append(w).append(",");
+        }
+
+        columnName_DataTypeString.deleteCharAt(columnName_DataTypeString.lastIndexOf(","));
+
+        try {
+            statement.execute("CREATE TABLE "+tableName+"("+columnName_DataTypeString+" )");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Table created successfully!");
+    }
+
+    // Insert into tablename (columnName1, columnName2 ...) values (value1, value2, ...)
+    public static void insertIntoTable(String tablename, String... columnName_Values){
+
+        StringBuilder columnName = new StringBuilder("");
+        StringBuilder value = new StringBuilder("");
+
+
+        for (String w : columnName_Values){
+                columnName.append(w.split(" ")[0]).append(",");
+                value.append(w.split(" ")[1]).append(",");
+        }
+
+        columnName.deleteCharAt(columnName.lastIndexOf(","));
+        value.deleteCharAt(value.lastIndexOf(","));
+
+        String command = "Insert into " + tablename +  "(" + columnName + ")" + "values (" + value + ")";
+
+        try {
+            statement.execute(command);
+            System.out.println("Data inserted successfully into " + tablename);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+}
